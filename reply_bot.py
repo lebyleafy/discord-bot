6d8fe9b4f20931570f014d722dac2ec8ca8fda48
leby <73567807+lebyleafy@users.noreply.github.com>
2021-08-16 07:51:24 +0700

    this is kinda inappropriate

---
+++ reply_bot.py
@@ -0,0 +1,115 @@
+#text reply
+leafyquote = [
+    "toxicity don't lose the game, bad players do", "fuck you HQ, gay bitch",
+    "if you're top frag and talk trash no one gonna talk back",
+    "please enjoy CS"
+]  #quote
+
+greetings = ["hi", "hello", "sup", "yo", "Hello", "Hi", "hey"]  #client chat
+
+greetings_back = [
+    "yeah yeah idc", "hello my ass LOL",
+    "dude go offline no ones care abt you except me", "bye"
+]  #bot reply
+
+say_bye = ["bye", "goodbye", "imma go offline", "getting off"]  #client chat
+
+bye = ["fuck off", "did i ask?", "don't comeback"]  #bot reply
+
+how_are_you = [
+    "how are you BOT?", "how are you bot", "how are you? bot",
+    "what's popping bot?", "how's everything bot?", "u good bot?",
+    "what's popping bot", "how are you bot?", "how are you BOT",
+    "how are you bot", "how are you BOT leafy?", "how are you BOT leafy",
+    "How are you BOT leafy?", "How are you BOT leafy", "how are you bot leafy",
+    "how are you bot leafy?", "How are you bot leafy", "How are you bot leafy?"
+]  #client chat
+
+how_are_you_reply = [
+    "meh i'm a bot why u ask me like that lol",
+    "i not sure how i'm feeling rn, but 1 think i'm sure abt is ur mom gay",
+    "i don't have emotion bc dumb leafy doesnt gib me a.i power but i'm good i guess"
+]  #bot reply
+
+bad_chat = [
+    "fuck you", "fuck u", "fack u", "fack you", "fak u", "fak you", "fuck yu",
+    "fk you", "fk u", "fck u", "fck you", "Fuck you", "Fck you"
+]  #client chat
+
+bad_reply = [
+    "ur mom", "fucking bitch, why u toxic?", "lol nice", "how about his mom?",
+    "why not urself lol", "gay shit why u wanted to fuck a guy", "men u gay",
+    "imagine being an asshole"
+]  #bot reply
+
+fuck = [
+    "fuck", "fck", "fack", "fucker", "fak", "fk", "fuk", "fac", "Fuck", "Fak",
+    "Fck"
+]
+
+fuck_reply = [
+    "nou", "yes fuck me daddy", "omg pls", "haha go do it urself",
+    "imagine being toxic", "gay", "i eat ass too"
+]
+
+roast_ppl = [
+    "'s family tree must be a catus because everybody on it is a prick.",
+    " is the reason God created the middle finger.",
+    " is so annoying he makes his Happy Meal cry.",
+    "'s face makes onions cry.",
+    " skipped 200 B.C update.",
+    " doesn't have a brain, i don't think he understand what we are doing",
+]  #roast
+
+wtf = [
+    "WTF", "wtf", "WATAFACK", "tf", "what the fuck", "What the fuck", "WTF?",
+    "wtf?", "WATAFACK?", "tf?", "what the fuck?", "What the fuck?", "TF",
+    "TF?", "Wtf", "Wtf?"
+]
+
+wtf_reply = [
+    "no idea", "wtf", "i dont know either", "why u so dumb", "ehhh idk", "tf",
+    "hmmm"
+]
+
+stfu = ['stfu','shut the fuck up','STFU','Stfu','Shut the fuck up','shut','shut up','Shut','SHUT','saddap','shatap','SHUT UP','Shut up']
+
+stfu_reply = ["how rude",'why dont you do it by yourself','i think you are the one who should shut up','no u','woa! be nice to your friend, prick','you too!']
+
+who_are_you = [
+    "who are you leafy", "who are you bot leafy", "who are you BOT leafy",
+    "Who are you leafy", "Who are you bot leafy", "Who are you BOT leafy",
+    "who are you leafy?", "who are you bot leafy?", "who are you BOT leafy?",
+    "Who are you leafy?", "Who are you bot leafy?", "Who are you BOT leafy?"
+]
+
+who_are_you_reply = [
+    "uh i am a bot lol",
+    "I'm leafy but a little smarter than who created me XD",
+    "I'm bot u dumbass", "weird question, isnt that too obvious that im a bot?"
+]
+
+who_is_leafy = [
+    "who is leafy", "who is leafy?", "Who is leafy?", "Who is leafy",
+    "who's leafy", "Who's leafy", "who's leafy?", "Who's leafy?", "Whos leafy",
+    "whos leafy", "whos leafy?", "Whos leafy?", "who leafy", "who leafy?",
+    "Who leafy", "Who leafy?", "leafy?", "Leafy?"
+]
+
+who_is_leafy_reply = [
+    "he's the greatest man, he created me||(i was programmed to say this thing)||",
+    "hes kinda cool he plays csgo and does some coding",
+    "hes programmer who created me",
+    "he's handsome and badass, real THE SHIT||(he told me to say that he's not that cool ya know XD)||"
+]
+
+praise = [
+    'you have the "*biggest*" cock I''ve ever seen',
+    "you are really handsome and smart! ~~i bet no one cares about you so you find me~~",
+    "how about you find a ""real"" person to praise you? oh wait you cant' find one lol, btw hope you have best luck and happy life <3",
+    "how about no?",
+    "shut up, you don't worth my praise",
+    "nah man, try go outside and touch some grass. Maybe you can find someone to praise you",
+    "you are funnier than SOME woman!"
+    "imagine asking a bot to praise you"
+]
